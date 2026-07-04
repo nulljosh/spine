7584175381dfe3dec24d16c47458d1cff3aba898
Joshua Trommel <trommatic@icloud.com>
2026-07-04 01:28:55 -0700

    Add macOS target sharing the WebView wrapper with iOS

--- ios/BooksApp/WebView.swift
+++ ios/BooksApp/WebView.swift
@@ -2,6 +2,19 @@ import SwiftUI
 import WebKit
 
 // ponytail: mirrors the live site instead of re-implementing its UI natively
+#if os(macOS)
+struct WebView: NSViewRepresentable {
+    let url: URL
+
+    func makeNSView(context: Context) -> WKWebView {
+        WKWebView()
+    }
+
+    func updateNSView(_ webView: WKWebView, context: Context) {
+        webView.load(URLRequest(url: url))
+    }
+}
+#else
 struct WebView: UIViewRepresentable {
     let url: URL
 
@@ -13,3 +26,4 @@ struct WebView: UIViewRepresentable {
         webView.load(URLRequest(url: url))
     }
 }
+#endif
